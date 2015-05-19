20a22e8c244b79397c06da8b13f51239a8ea8268
Patrick Williams <iawillia@us.ibm.com>
2015-05-19 17:57:11 -0500

    Add a few new tools to wrappers:
        - ar as cpp objcopy

--- create_wrapper_links.sh
+++ create_wrapper_links.sh
@@ -2,7 +2,7 @@
 
 CROSS_PREFIX="powerpc64-unknown-linux-gnu-"
 HOST_PREFIX="x86_64-pc-linux-gnu-"
-GCC_TOOLS="gcc g++ ld objdump readelf nm gcov"
+GCC_TOOLS="gcc g++ ld objdump readelf nm gcov ar as cpp objcopy"
 
 WRAPPERS="yacc bison lex flex pahole"
 
